e1d75ed9a04e2a88821203740662d8f4cfe3fbd2
evertonsantos17-blip <everton_santos.17@outlook.com>
2026-09-24 11:01:45 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC75miCBCfVO4efC8qE78RtH3ZwEDToWo8",
+  authDomain: "reducao-de-despesas.firebaseapp.com",
+  projectId: "reducao-de-despesas",
+  storageBucket: "reducao-de-despesas.firebasestorage.app",
+  messagingSenderId: "63711041653",
+  appId: "1:63711041653:web:078e9b4cd7e2982f30003c"
+};
